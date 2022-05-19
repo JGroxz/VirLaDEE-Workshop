d4a874ff7521dfd2651f90c79b1e78a222c66cc2
Yevhen Bondarenko <zekbond@gmail.com>
2022-05-19 17:33:47 +0300

    Add player color setter

---
+++ Assets/[Scripts]/PlayerColorSetter.cs
@@ -0,0 +1,49 @@
+using System;
+using Mirage;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class PlayerColorSetter : NetworkBehaviour
+{
+    public Color generatedColor;
+    public float from = 0.3f;
+    public float to = 0.8f;
+
+    public MeshRenderer renderer;
+
+    public Color GetRandomColor()
+    {
+        return new Color
+        {
+            r = Random.Range(from, to),
+            g = Random.Range(from, to),
+            b = Random.Range(from, to),
+            a = 1
+        };
+    }
+
+    private void Start()
+    {
+        generatedColor = GetRandomColor();
+        if (IsLocalPlayer) RequestSetColor(generatedColor);
+    }
+    
+    [ServerRpc]
+    private void RequestSetColor(Color color)
+    {
+        // extra logic
+        AcceptSetColor(color);
+    }
+
+    [ClientRpc]
+    private void AcceptSetColor(Color color)
+    {
+        generatedColor = color;
+        UpdateMaterialColor();
+    }
+
+    private void UpdateMaterialColor()
+    {
+        renderer.material.color = generatedColor;
+    }
+}
